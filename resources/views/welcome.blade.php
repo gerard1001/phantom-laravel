61c75feccea14450a97795435f92f7204bd57353
gerard1001 <rutayisiregerard2002@gmail.com>
2024-02-11 09:02:25 +0200

    Revert "Init commit, all tables done."
    This reverts commit f227f3b970bb2440b7299db671e4eaa0a8c13ad0.

--- resources/views/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>{{$heading}}</h1>
-@foreach($listings as $listing)
-    <a href="/listings/{{$listing['id']}}"><h2>{{$listing['title']}}</h2></a>
-    <p>{{$listing['description']}}</p>
-    <hr>
-@endforeach
